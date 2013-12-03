ac1df2097125d22ac266e31a8baab0528b7fc4fd
zhangkangen <zhangkangen@gmail.com>
2013-12-03 16:44:38 +0800

    AddFileManage

---
+++ _05资料管理器/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace _05资料管理器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tvRoot = new System.Windows.Forms.TreeView();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tvRoot);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtWord);
+            this.splitContainer1.Size = new System.Drawing.Size(630, 485);
+            this.splitContainer1.SplitterDistance = 210;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tvRoot
+            // 
+            this.tvRoot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvRoot.Location = new System.Drawing.Point(0, 0);
+            this.tvRoot.Name = "tvRoot";
+            this.tvRoot.Size = new System.Drawing.Size(210, 485);
+            this.tvRoot.TabIndex = 0;
+            this.tvRoot.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tvRoot_NodeMouseDoubleClick);
+            // 
+            // txtWord
+            // 
+            this.txtWord.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtWord.Location = new System.Drawing.Point(0, 0);
+            this.txtWord.Multiline = true;
+            this.txtWord.Name = "txtWord";
+            this.txtWord.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtWord.Size = new System.Drawing.Size(416, 485);
+            this.txtWord.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 485);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView tvRoot;
+        private System.Windows.Forms.TextBox txtWord;
+    }
+}
+
---
+++ _05资料管理器/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05资料管理器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 窗体加载时加上资料管理器
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = Assembly.GetEntryAssembly().Location;
+            string dirPath = Path.Combine(Path.GetDirectoryName(path), "demo");
+            LoadDir(dirPath, tvRoot.Nodes);
+        }
+
+        private void LoadDir(string dirPath, TreeNodeCollection treeNodeCollection)
+        {
+            //获取当前目录下的所有文件夹
+            string[] folderPath = Directory.GetDirectories(dirPath);
+            foreach (string item in folderPath)
+            {
+                string folderName = Path.GetFileName(item);
+                TreeNode tn = treeNodeCollection.Add(folderName);
+                LoadDir(item, tn.Nodes);
+            }
+            string[] filePaths = Directory.GetFiles(dirPath);
+            foreach (string item in filePaths)
+            {
+                string fileName = Path.GetFileName(item);
+                TreeNode tn = treeNodeCollection.Add(fileName);
+                //节点的tag存储文件的路径
+                tn.Tag = item;
+            }
+        }
+        /// <summary>
+        /// 双击显示文本中的内容
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tvRoot_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            string filePath = tvRoot.SelectedNode.Tag.ToString();
+            txtWord.Text = File.ReadAllText(filePath, Encoding.Default);
+        }
+
+    }
+}
